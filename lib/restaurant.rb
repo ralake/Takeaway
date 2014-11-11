9a62a152ef7377d48de7b7aedc8422a92f9f4993
Richard Lake <ralake1985@gmail.com>
2014-11-11 23:35:01 +0000

    Add receive & process order methods to Restaurant class

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,17 +1,31 @@
 class Restaurant
 
-  attr_reader :menues
+  attr_reader :menues, :orders
 
   def initialize
     @menues = []
+    @orders = []
   end
 
   def add_menu(menu)
     menues << menu
   end
 
+  def remove_menu(menu)
+    menues.delete(menu)
+  end
+
   def has_menu?
     !menues.empty?
+  end
+
+  def receive_order(order)
+    orders << order
+  end
+
+  def process_order(order)
+    orders.delete(order)
+    order.send_confirmation
   end
 
 end
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -4,6 +4,7 @@ describe Restaurant do
 
   let(:the_exotic_viscera_emporium) { Restaurant.new }
   let(:menu) { double :Menu }
+  let(:first_order) { double :Order}
 
   it 'can hold menues' do
     expect(the_exotic_viscera_emporium.menues).to be_an_instance_of(Array)
@@ -14,8 +15,26 @@ describe Restaurant do
     expect(the_exotic_viscera_emporium.has_menu?).to be true
   end
 
+  it 'can remove a menu' do
+    the_exotic_viscera_emporium.add_menu(menu)
+    the_exotic_viscera_emporium.remove_menu(menu)
+    expect(the_exotic_viscera_emporium.has_menu?).to be false
+  end
+
   it 'knows if it does not have a menu' do
     expect(the_exotic_viscera_emporium).not_to have_menu
+  end
+
+  it 'can receive and order' do
+    the_exotic_viscera_emporium.receive_order(first_order)
+    expect(the_exotic_viscera_emporium.orders).to eq([first_order])
+  end
+
+  it 'can process an order' do
+    allow(first_order).to receive(:send_confirmation)
+    the_exotic_viscera_emporium.receive_order(first_order)
+    the_exotic_viscera_emporium.process_order(first_order)
+    expect(the_exotic_viscera_emporium.orders).to eq([])
   end
 
 end
