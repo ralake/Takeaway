dd489ca5b39b08d4d249ba5650bc64fab6bd4b18
Richard Lake <ralake1985@gmail.com>
2014-11-11 22:47:22 +0000

    Add Menu class

---
+++ lib/menu.rb
@@ -0,0 +1,17 @@
+class Menu
+
+  attr_reader :dish_list
+
+  def initialize
+    @dish_list = []
+  end
+
+  def add_dish(dish)
+    dish_list << dish
+  end
+
+  def remove_dish(dish)
+    dish_list.delete(dish)
+  end
+
+end
---
+++ spec/menu_spec.rb
@@ -0,0 +1,25 @@
+require './lib/menu'
+
+describe Menu do
+
+  let(:menu) { Menu.new }
+  let(:parrot_brains) { double :Dish }
+  let(:capybara_eyeballs) { double :Dish }
+
+  it 'should have a list of its dishes' do
+    expect(menu.dish_list).to eq([])
+  end
+
+  it 'should be able to add a dish' do
+    menu.add_dish(parrot_brains)
+    expect(menu.dish_list).to eq([parrot_brains])
+  end
+
+  it 'should be able to remove a dish' do
+    menu.add_dish(parrot_brains)
+    menu.add_dish(capybara_eyeballs)
+    menu.remove_dish(parrot_brains)
+    expect(menu.dish_list).to eq([capybara_eyeballs])
+  end
+
+end
